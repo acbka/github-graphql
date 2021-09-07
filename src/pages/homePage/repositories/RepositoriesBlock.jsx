70fdc27127fe488412b6a3808d1016bf5f799de5
acbka <vladochka.vb@gmail.com>
2021-09-08 09:48:32 +1200

    Add repositories sorting

--- src/pages/homePage/repositories/RepositoriesBlock.jsx
+++ src/pages/homePage/repositories/RepositoriesBlock.jsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled/macro";
 import Repository from "./Repository";
 import RepositoryInfo from "./RepositoryInfo";
 import { Block, Title } from "../style";
+import _ from "lodash";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -23,15 +24,29 @@ const Header = styled.div`
   border-bottom: 2px solid var(--color-dark);
   font-size: 16px;
 `;
+const HeaderTitle = styled.div`
+  cursor: pointer;
+`;
 const RepositoriesArray = styled.div`
   height: 100%;
   overflow: auto;
 `;
 
-const RepositoriesBlock = ({ repositories, user }) => {
+const RepositoriesBlock = ({ repositories }) => {
   const [selected, setSelected] = useState(null);
+  const [sortedRepositories, setSortedRepositories] = useState(repositories);
+  const [sortKey, setSortKey] = useState("");
 
-  const list = repositories.map((item, index) => {
+  const sortArray = (value) => {
+    const newArray =
+      sortKey === value
+        ? [...sortedRepositories].reverse()
+        : _.sortBy(sortedRepositories, value);
+    setSortedRepositories(newArray);
+    setSortKey(value);
+  };
+
+  const list = sortedRepositories.map((item, index) => {
     return (
       <Repository
         key={index}
@@ -48,10 +63,23 @@ const RepositoriesBlock = ({ repositories, user }) => {
         <Block>
           <Title>Repositories</Title>
           <Header>
-            <div>Name</div>
-            <div>Created At</div>
-            <div>Updated At</div>
+            <HeaderTitle onClick={() => sortArray("name")}>Name</HeaderTitle>
+            <HeaderTitle onClick={() => sortArray("createdAt")}>
+              Created At
+            </HeaderTitle>
+            <HeaderTitle onClick={() => sortArray("updatedAt")}>
+              Updated At
+            </HeaderTitle>
           </Header>
+          {/* <Header>
+            <HeaderTitle onClick={sortArray}>Name</HeaderTitle>
+            <HeaderTitle onClick={sortArray}>
+              Created At
+            </HeaderTitle>
+            <HeaderTitle onClick={sortArray}>
+              Updated At
+            </HeaderTitle>
+          </Header> */}
           <RepositoriesArray>{list}</RepositoriesArray>
         </Block>
       </RepositoriesList>
--- src/pages/homePage/user/GetUser.jsx
+++ src/pages/homePage/user/GetUser.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { USER_QUERY } from "../../../queries/userQuery";
 import UserInfo from "./UserInfo"
+import Spinner from "../../../components/Spinner";
 
 const GetUser= ({ login, getNumberOfRepositories  }) => {
   const { loading, error, data } = useQuery(USER_QUERY, {
     variables: { login: login },
   });
-  if (loading) return <></> ;
+  if (loading) return <Spinner /> ;
   if (error) return <p>User doesn't exist.</p>;
   const user = data.user;
   getNumberOfRepositories(user.repositories.totalCount);
